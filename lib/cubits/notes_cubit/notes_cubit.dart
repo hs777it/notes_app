e42c655e2d217df89e9b4f0754bcf5aac124e9b4
hs07 <hs777it@gmail.com>
2023-04-15 22:08:35 +0200

    create fetch notes method and trigger notes cubit

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,16 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
-
-import '../../constants.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
 
-  fetchAllNotes() async {
+  fetchAllNotes() {
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
 
